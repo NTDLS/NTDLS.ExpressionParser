eb80c5fe524d182a7e6c9095c725d5dd0092a32c
Josh Patterson <josh@ntdls.com>
2023-10-17 06:36:07 -0400

    Readability.

--- TestHarness/Program.cs
+++ TestHarness/Program.cs
@@ -9,51 +9,40 @@ namespace TestApp.CSharp
             var result = Expression.Evaluate("10 * ((5 + 1000 + ( 10 + !0 )) * Ceil(SUM(11.6, 12.5, 14.7, 11.11)) + 60.5) * 10", out string work);
             Console.WriteLine(work);
 
-
             //("10 * ((5 + extra + ( 10 + !0 )) * Ceil(SUM(11.6, 12.5, 14.7, 11.11)) + 60.5) * 10": 5,086,050 when "extra" is 1000
-            /*
-                        var expression = new Expression("10 * ((5 + extra + DoStuff(11,55) + ( 10 + !0 )) * Ceil(SUM(11.6, 12.5, 14.7, 11.11)) + 60.5) * 10");
+            var expression = new Expression("10 * ((5 + extra + DoStuff(11,55) + ( 10 + !0 )) * Ceil(SUM(11.6, 12.5, 14.7, 11.11)) + 60.5) * 10");
 
-                        expression.AddParameter("extra", 1000);
+            expression.AddParameter("extra", 1000);
 
-                        expression.AddFunction("DoStuff", (double[] parameters) =>
-                        {
-                            double sum = 0;
-
-                            foreach (var parameter in parameters)
-                            {
-                                sum += parameter;
-                            }
-
-                            return sum;
-                        });
-            */
-            /*
-            var expression = new Expression("10 * ((5 + 1000 + ( 10 + !0 )) + 60.5) * 10");
-            //var expression = new Expression("-10+50");
-
-            DateTime startDate = DateTime.Now;
-            for (int i = 0; i < 500000; i++)
+            expression.AddFunction("DoStuff", (double[] parameters) =>
             {
-                var result = expression.Evaluate();
-            }
-            Console.WriteLine($"NTDLS: {(DateTime.Now - startDate).TotalMilliseconds}");
+                double sum = 0;
 
-            //Console.WriteLine($"Result: {result}");
-            */
+                foreach (var parameter in parameters)
+                {
+                    sum += parameter;
+                }
 
-            string strExpr = "10 * ((5 + 1000 + ( 10 )) *  60.5) * 10"; //Need to hit 160ms with 100000 iterations
+                return sum;
+            });
 
-            var ntdlsExpression = new Expression(strExpr);
+            Perform("10 * ((5 + 1000 + ( 10 )) *  60.5) * 10");
 
-            DateTime startDate = DateTime.Now;
-            //for (int i = 0; i < 100000; i++)
-            {
-                ntdlsExpression.Evaluate();
-            }
-            Console.WriteLine($"NTDLS: {(DateTime.Now - startDate).TotalMilliseconds}");
             Console.ReadLine();
+        }
 
+        static void Perform(string expr)
+        {
+            Console.Write($"[{expr}] -> ");
+
+            var expression = new Expression(expr);
+
+            DateTime startDate = DateTime.Now;
+            for (int i = 0; i < 100000; i++)
+            {
+                expression.Evaluate();
+            }
+            Console.WriteLine($"{(DateTime.Now - startDate).TotalMilliseconds:n2}");
         }
     }
 }
