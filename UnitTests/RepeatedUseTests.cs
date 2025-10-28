2f2698d03d550c29a62052d57c2b2f806df4552c
Josh Patterson <Josh@NTDLS.com>
2025-10-28 13:56:08 -0400

    More unit tests.

---
+++ UnitTests/RepeatedUseTests.cs
@@ -0,0 +1,22 @@
+﻿using NTDLS.ExpressionParser;
+
+namespace UnitTests
+{
+    public class RepeatedUseTests
+    {
+        [Fact]
+        public void Simple()
+        {
+            var expr = new Expression("v1");
+
+            expr.SetParameter("v1", 12345);
+            Console.WriteLine(expr.Evaluate());
+            expr.SetParameter("v1", 12345);
+            Console.WriteLine(expr.Evaluate());
+            expr.SetParameter("v1", 12345);
+            Console.WriteLine(expr.Evaluate());
+            expr.SetParameter("v1", 12345);
+            Console.WriteLine(expr.Evaluate());
+        }
+    }
+}
